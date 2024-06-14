0edfec300a0863c0be57468df92ce3338da9e401
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-14 16:55:03 -0300

    Envio do formulário

--- formularios/formularios/src/components/MyForm.jsx
+++ formularios/formularios/src/components/MyForm.jsx
@@ -10,13 +10,20 @@ const MyForm = () => {
         setName(e.target.value)
     }
 
-    console.log(name)
-    console.log(email)
+    // console.log(name)
+    // console.log(email)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando o formulário")
+        console.log(name, email)
+    }
 
   return (
     <div>
+        {/* 5 - envio de forms */}
         {/* 1 - criaçaõ de form */}
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName}/>
@@ -24,6 +31,7 @@ const MyForm = () => {
             {/* 2 - label envolvendo input */}
             <label>
                 <span>E-mail</span>
+                {/* 4 - Simplificação de manipulação de state */}
                 <input type="email" name='email' placeholder='Digite o seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
             </label>
             <input type="submit" value="Enviar" />
