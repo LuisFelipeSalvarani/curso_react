7c5ac9ac12c24208d3daf29c2e4e5a91933ea2ad
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-14 14:45:10 -0300

    CSS de componente

--- css_react/css_react/src/App.jsx
+++ css_react/css_react/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* CSS global */}
       <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
     </>
   )
 }
