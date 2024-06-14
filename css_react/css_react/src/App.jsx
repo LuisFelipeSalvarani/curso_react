a96afa3d0bebc2b2496eeb8bafcd6802d0b9bb88
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-14 14:52:20 -0300

    CSS inline dinâmico

--- css_react/css_react/src/App.jsx
+++ css_react/css_react/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const n = 15
+  const [name] = useState("Matheus")
 
   return (
     <>
@@ -14,9 +15,13 @@ function App() {
       {/* CSS de componente */}
       <MyComponent />
       <p>Este parágrafo é do App.js</p>
-      {/* Inline CSS */}
+      {/* CSS Inline */}
       <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
       <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+      {/* CSS Inline dinâmico */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinâmico</h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinâmico</h2>
+      <h2 style={name === "Matheus" ? ({color: "green", backgroundColor: "#000"}) : null}>Teste nome</h2>
     </>
   )
 }
