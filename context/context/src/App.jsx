60fbf2e451f9dc0783f5e665d18a06a27b489e69
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-18 12:50:07 -0300

    Dasafio 8

--- context/context/src/App.jsx
+++ context/context/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
 import './App.css'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+
+// pages
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Products from './pages/Products'
 
 function App() {
 
   return (
     <>
       <h1>Context API</h1>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
