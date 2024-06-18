06b5914c51aa16b6d92dcf8b4198ba748a2c8896
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-06-17 23:11:05 -0300

    Navegação entre páginas

--- router/router/src/App.jsx
+++ router/router/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
+import Navbar from './components/Navbar'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <h1>React Router</h1>
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
