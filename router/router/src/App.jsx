a9263245040bc42230e41be046ea14c1b5b93666
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-06-17 23:04:29 -0300

    Configuração do React Router

--- router/router/src/App.jsx
+++ router/router/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 
+// 1 - config react router
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
 
   return (
     <>
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
