2beb057c5eb08ae80ee6f1575a444fe0c6aa0d48
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-06-17 23:38:02 -0300

    Rotas dinâmicas

--- router/router/src/App.jsx
+++ router/router/src/App.jsx
@@ -3,10 +3,13 @@ import './App.css'
 // 1 - config react router
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// components
+import Navbar from './components/Navbar'
+
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
-import Navbar from './components/Navbar'
+import Product from './pages/Product'
 
 function App() {
 
@@ -19,6 +22,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          {/* 4 - rota dinâmica */}
+          <Route path='/products/:id' element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
