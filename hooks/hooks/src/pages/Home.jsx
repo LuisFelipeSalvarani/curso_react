3fe518b10cc0ac571c95b26f99083d39cf62ca99
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-20 17:36:35 -0300

    useCallback

--- hooks/hooks/src/pages/Home.jsx
+++ hooks/hooks/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import HookUseEffect from '../components/HookUseEffect'
 import HookUseReducer from '../components/HookUseReducer'
 import HookUseState from '../components/HookUseState'
+import HookUseRef from '../components/HookUseRef'
+import HookUseCallback from '../components/HookUseCallback'
 
 // useContext
 import { useContext } from 'react'
 import { SomeContext } from '../components/HookUseContext'
-import HookUseRef from '../components/HookUseRef'
 
 const Home = () => {
     const { contextValue } = useContext(SomeContext)
@@ -19,6 +20,7 @@ const Home = () => {
         <p>Valor do context: {contextValue}</p>
         <hr />
         <HookUseRef />
+        <HookUseCallback />
     </div>
   )
 }
